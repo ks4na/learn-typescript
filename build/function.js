becad6752da13b16e83e6b3f958476a4a202b617
lvyou <lv_you@ks.hyron.com>
2019-09-25 21:44:49 +0800

    update function.md, need to update the last part

--- build/function.js
+++ build/function.js
@@ -19,3 +19,42 @@ var myAdd2 = function (x, y) {
 var myAdd3 = function (x, y) {
     return x + y;
 };
+// 可选参数和默认参数
+// 1. 可选参数
+function printPerson(name, age) {
+    return "Person:: name: " + name + ", age: " + age;
+}
+console.log(printPerson('yuusha', 24));
+console.log(printPerson('ks4na'));
+// 2. 默认参数
+function printPerson2(name, age) {
+    if (name === void 0) { name = 'defaultName'; }
+    console.log("Person:: name: " + name + ", age: " + age);
+}
+printPerson2('yuusha2', 24);
+printPerson2(undefined, 23);
+// 剩余参数
+function printNum(num) {
+    var restNums = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        restNums[_i - 1] = arguments[_i];
+    }
+    console.log(num);
+    console.log(Array.isArray(restNums), restNums);
+}
+printNum(1);
+printNum(1, 2, 3);
+var printNumber = printNum;
+printNumber = null;
+var person = {
+    name: 'yuusha',
+    age: 24,
+    printInfo: function () {
+        console.log(this.name, this.age);
+    }
+};
+person.printInfo();
+// let foo = person.printInfo
+// foo()  // Error
+var foo = person.printInfo.bind({ name: 'xyz', age: 123, printInfo: function () { } });
+foo(); // OK, `this` refers to an  `UseThisDemo` object
--- src/function.ts
+++ src/function.ts
@@ -28,3 +28,67 @@ let myAdd2 = function(x: number, y: number): number {
 let myAdd3: (x: number, y: number) => number = function(x, y) {
   return x + y
 }
+
+// 可选参数和默认参数
+// 1. 可选参数
+function printPerson(name: string, age?: number): string {
+	return `Person:: name: ${name}, age: ${age}`
+}
+
+console.log(printPerson('yuusha', 24))
+console.log(printPerson('ks4na'))
+
+// 2. 默认参数
+function printPerson2(name = 'defaultName', age: number): void {
+	console.log(`Person:: name: ${name}, age: ${age}`)
+}
+
+printPerson2('yuusha2', 24)
+printPerson2(undefined, 23)
+
+// 剩余参数
+function printNum(num: number, ...restNums: number[]): void {
+	console.log(num)
+	console.log(Array.isArray(restNums), restNums)
+}
+
+printNum(1)
+
+printNum(1,2,3)
+
+let printNumber: ((a: number, ...rest: number[]) => void) | null = printNum
+printNumber = null
+
+// This 
+// let person = {
+	// age: 12,
+	// printAge: function (){
+		// return function (){
+			// console.log(this.age)
+		// }
+	// }
+// }
+
+// person.printAge()
+
+interface UseThisDemo {
+	name: string
+	age: number
+	printInfo: (this: UseThisDemo) => void
+}
+
+let person: UseThisDemo = {
+	name: 'yuusha',
+	age: 24,
+	printInfo: function (this: UseThisDemo){
+		console.log(this.name, this.age)
+	}
+}
+
+person.printInfo()
+// let foo = person.printInfo
+// foo()  // Error
+let foo = person.printInfo.bind({name: 'xyz', age: 123, printInfo(){}})
+foo()  // OK, `this` refers to an  `UseThisDemo` object
+
+
